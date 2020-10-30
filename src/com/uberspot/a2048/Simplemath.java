f43b6cb54d6313a89fd41e99cf3948d6945bc2d7
Chao <tilb0023@hz.nl>
2020-10-30 15:42:18 +0100

    unit testing

---
+++ src/com/uberspot/a2048/Simplemath.java
@@ -0,0 +1,18 @@
+package com.uberspot.a2048;
+
+public class Simplemath {
+
+    public int add(int op1, int op2) {
+        return op1 + op2;
+    }
+
+    public int diff(int op1, int op2) {
+        return op1 - op2;
+    }
+
+    public double div(int op1, int op2) {
+        // if (op2 == 0) return 0;
+        return op1 / op2;
+    }
+
+}
---
+++ src/test/java/com/uberspot/a2048/SimplemathTest.java
@@ -0,0 +1,21 @@
+package com.uberspot.a2048;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SimplemathTest {
+
+    @Test
+    public void add() {
+        Simplemath simplemath = new Simplemath();
+        assertEquals(simplemath.add(1, 2), 3);
+    }
+
+    @Test
+    public void diff() {
+    }
+
+    @Test
+    public void div() {
+    }
+}
